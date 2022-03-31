736562220c8e3481f3fdbc20220b3e379ea2cf37
ibmpariwesh <ibmpariwesh@gmail.com>
2022-03-31 22:45:10 +0530

    swagger support

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,12 @@ public class UserController {
 	UserService service;
 //	@Autowired
 //	RestTemplate template;
+	@GetMapping
+	void getUser() {
+		// TODO Auto-generated method stub
 
+	}
+	
 	@PostMapping
 	Integer createUser(@RequestBody User user) {
 		if (user == null) {
